dc14a133afd0205f479f2994cbe89d9827abe8a3
Gustavo Calca <gstv.calca@gmail.com>
2025-04-21 14:15:34 +1000

    fix: added host 0000 config on the app

--- src/server.ts
+++ src/server.ts
@@ -16,7 +16,7 @@ const app = fastify({ logger: true });
 
 const start = async () => {
   try {
-    await app.listen({ port: env.PORT});
+    await app.listen({ port: env.PORT, host: "0.0.0.0"});
   } catch (error) {
     app.log.error(error);
     process.exit(1);
