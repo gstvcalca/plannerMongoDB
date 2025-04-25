1cca4aeaa0ac0018974561902d24c17cb79f19d1
Gustavo Calca <gstv.calca@gmail.com>
2025-04-26 02:20:02 +1000

    fix: changed get users endpoint

--- src/routes/get-users.ts
+++ src/routes/get-users.ts
@@ -2,7 +2,7 @@ import { FastifyInstance } from 'fastify';
 import { User } from '../models/user-model';
 
 export async function getUsers(app: FastifyInstance) {
-  app.get('/api/users', async (req, reply) => {
+  app.get('/users', async (req, reply) => {
     const users = await User.find();
     return { users };
   });
